c64bd03ef6f08a1f3c11c4af880edb8dfa241096
unknown <faisaljfcl@gmail.com>
2023-02-10 17:40:43 +0600

    Deactivation Request will remove meal from Active List
    Now gotta prevent posting same instances multiple times
    in the Deactivation Requests and Consumed Meals Collection

--- Controllers/Deactivation_Request.js
+++ Controllers/Deactivation_Request.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Types;
 
 const router = express.Router();
 
 // Importing Models
 const DEACTIVATION_REQUESTS = require("../Models/DEACTIVATION_REQUESTS");
+const ACTIVE_MEAL = require("../Models/ACTIVE_MEALS");
 
 // Importing Payment Object Schema
 const deactivationRequestValidation = require("../FormValidators/DeactivationRequestSchema");
@@ -23,6 +26,21 @@ router.post("/", deactivationRequestValidation, async (req, res) => {
     const newDeactivationEntry = new DEACTIVATION_REQUESTS(req.body);
     const response = await newDeactivationEntry.save();
     response["message"] = "Successfully POSTED deactivation info";
+    const { card_no, deactivation_start_date, committee_no, User_id } =
+      req.body;
+    const query = {
+      card_no,
+      User_id: new ObjectId(User_id),
+      active_date: deactivation_start_date,
+    };
+    const isActivated = await ACTIVE_MEAL.findOne(query);
+    if (isActivated) {
+      const deleteResponse = await ACTIVE_MEAL.deleteOne({
+        _id: new ObjectId(isActivated._id),
+      });
+      response["message"] =
+        "Successfully POSTED deactivation info & Deleted from Active Meal List";
+    }
     return res.send(response);
   } catch (error) {
     console.error(error);
