e7e0071b4f8469b3279654cca2dda4096c223ec2
foy4748 <faisaljfcl@gmail.com>
2023-05-06 21:07:32 +0600

    Added Activation Request GET route

--- Controllers/Activation_Request.js
+++ Controllers/Activation_Request.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { validationResult } = require("express-validator");
+const { ObjectId } = mongoose.Types;
 
 const router = express.Router();
 
@@ -9,6 +11,21 @@ const ACTIVATION_REQUESTS = require("../Models/ACTIVATION_REQUESTS");
 // Importing Payment Object Schema
 const activationRequestValidation = require("../FormValidators/ActivationRequestSchema");
 const ErrorHandlingMW = require("../Middlewares/ErrorHandlingMW");
+
+router.get("/", async (req, res, nxt) => {
+  try {
+    const { card_no, user_id } = req.headers;
+    const records = await ACTIVATION_REQUESTS.find({
+      card_no,
+      User_id: new ObjectId(user_id),
+    });
+    res.send({ records });
+  } catch (error) {
+    res.status(501);
+    res.msg = "FAILED to  GET activation request info";
+    nxt(error);
+  }
+});
 
 router.post("/", activationRequestValidation, async (req, res, nxt) => {
   const error = validationResult(req).formatWith(({ msg }) => msg);
@@ -28,7 +45,7 @@ router.post("/", activationRequestValidation, async (req, res, nxt) => {
     return res.send(response);
   } catch (error) {
     res.status(501);
-    res.msg = "FAILED to  POSTED requestion activation info";
+    res.msg = "FAILED to  POSTED activation requestion info";
     nxt(error);
   }
 });
